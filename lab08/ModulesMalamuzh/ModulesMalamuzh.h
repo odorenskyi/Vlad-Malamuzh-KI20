29a922829a05f8a20dcbb3af516bbddadac1bd69
Vladyslav Malamuzh <79717231+vlad90435@users.noreply.github.com>
2021-04-14 18:55:15 +0300

    Delete lab08/ModulesMalamuzh directory

--- lab08/ModulesMalamuzh/ModulesMalamuzh.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULESMALAMUZH_H_INCLUDED
-#define MODULESMALAMUZH_H_INCLUDED
-double calculations_s(double , double , double);
-
-
-
-
-#endif // MODULESMALAMUZH_H_INCLUDED
--- lab08/ModulesMalamuzh/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-
-
-double calculations_s(double x, double y, double z)
-{
-    double s = M_PI*x+pow(M_E,abs(y))-sqrt(abs(pow(z,2)-y)) ;
-    return s;
-}
